890566f83b54555c4ee3595a299220e7d365d13f
QISUN000 <qsun4@rrc.ca>
2025-03-12 10:45:01 -0500

    1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+   <Counter title="Counter 1"/>
+  </div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Counter
   }
 }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+        <h2>Count: {{ count }}</h2>
+        <button @click="increment">+</button>
+        <button @click="decrement">-</button>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'Counter',
+    props: {
+        title: String,
+        default: ()=> 'Counter'
+    },
+    data(){
+        return{
+            count:0
+        }
+    },
+    methods:{
+        increment(){
+            this.count++
+        },
+        decrement(){
+            this.count--
+        }
+    }
+}
+</script>
